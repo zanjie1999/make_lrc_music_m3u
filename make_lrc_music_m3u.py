3dccdb56b34a63f84a941b2fc6b1d1da723f3b09
Sparkle <zanjie1999@yeah.net>
2017-10-20 23:26:46 -0700

    python make_lrc_music_m3u.py 21777546

---
+++ make_lrc_music_m3u.py
@@ -0,0 +1,122 @@
+# -*- encoding:utf-8 -*-
+
+# 网易云音乐 lrc歌曲m3u生成器
+# 版本: 2.0
+import codecs
+import hashlib
+import json
+import os
+import urllib
+import urllib2
+from sys import argv
+
+# 歌单id设置
+playlistId = argv[1]
+
+# 音乐存放位置
+mp3dir = u""
+
+# 播放列表存放位置
+m3udir = u""
+
+# 半角转全角
+def half2full(ustring):
+    rstring = ""
+    for uchar in ustring:
+        inside_code = ord(uchar)
+        if inside_code == 32:
+            inside_code = 12288
+        elif inside_code >= 32 and inside_code <= 126:
+            inside_code += 65248
+
+        rstring += unichr(inside_code)
+    return rstring
+
+
+# 替换文件名不允许字符
+def replaceName(name):
+    name = name.replace('?', half2full('?'))
+    name = name.replace('*', half2full('*'))
+    name = name.replace('/', half2full('/'))
+    name = name.replace('\\', half2full('\\'))
+    name = name.replace('<', half2full('<'))
+    name = name.replace('>', half2full('>'))
+    name = name.replace(':', half2full(':'))
+    name = name.replace('\"', half2full('\"'))
+    name = name.replace('|', half2full('|'))
+    return name
+
+
+# 获取歌词
+def getLrc(tracksId):
+    url = 'http://music.163.com/api/song/media?id=' + tracksId
+    dataS = json.load(urllib2.urlopen(url))
+    if dataL['code'] != 200:
+        ecode = bytes(dataL['code'])
+        print 'errorCode: ' + ecode
+    else:
+        # 判断是否有歌词
+        if dataS.has_key('lyric'):
+            return dataS['lyric']
+        else:
+            return ''
+
+
+# 下载歌曲
+def dowmMusic(tracksId, fileName):
+    url = 'http://music.163.com/song/media/outer/url?id=' + tracksId + '.mp3'
+    print 'Download music: ' + (fileName.encode('GBK', 'ignore'));
+    urllib.urlretrieve(url, fileName)
+
+
+# 写出文件
+def writeToFile(name, text):
+    print 'Write to file: ' + (name.encode('GBK', 'ignore'));
+    file = codecs.open(name, "w", "utf-8")
+    file.write(text)
+    file.close()
+
+
+# 生成播放列表
+m3uText = "#EXTM3U"
+def addPlaylist(mp3Title, mp3Name):
+    global m3uText
+    m3uText += u"\n#EXTINF:" + mp3Title + u"\n" + mp3dir + mp3Name
+
+
+# 获取歌单
+url = 'http://music.163.com/api/playlist/detail?id=' + playlistId
+dataL = json.load(urllib2.urlopen(url))
+if dataL['code'] != 200:
+    ecode = bytes(dataL['code'])
+    print 'errorCode: ' + ecode
+else:
+    # 循环歌单
+    allNum = len(dataL['result']['tracks'])
+    nowNum = 0
+    for tracks in dataL['result']['tracks']:
+        nowNum += 1
+        print bytes(nowNum) + '/ ' + bytes(allNum)
+        fileName = ''
+        i = len(tracks['artists']) - 1
+        for artist in tracks['artists']:
+            if i > 0:
+                fileName += artist['name'] + u","
+                i -= 1
+            else:
+                fileName += artist['name']
+        fileName += u" - " + tracks['name']
+        fileName = replaceName(fileName)
+        tid = bytes(tracks['id'])
+        # 存在歌曲文件跳过
+        if not os.path.isfile(m3udir + fileName + u".mp3"):
+            dowmMusic(tid, m3udir + fileName + u".mp3")
+        # 存在歌词文件就跳过
+        if not os.path.isfile(m3udir + fileName + u".lrc"):
+            lrcS = getLrc(tid)
+            if len(lrcS) > 0:
+                writeToFile(m3udir + fileName + u".lrc", lrcS)
+        # 添加到播放列表
+        addPlaylist(tracks['name'], fileName + u".mp3")
+    # 写播放列表文件
+    writeToFile(m3udir + dataL['result']['name'] + u".m3u", m3uText)
